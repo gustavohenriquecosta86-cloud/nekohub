d66e4a8548213143c883c3770d8f8899cb56fe34
gustavohenriquecosta86-cloud <gustavohenriquecosta86@gmail.com>
2026-04-20 12:44:21 -0300

    Add user login functionality with chat feature

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+const users = {
+  "G27S72": "Gustavo 🌟",
+  "S58S78": "Sarah ❤️",
+  "J37L47": "Jamilly 🧒",
+  "A-WI299": "André 🧑"
+};
+
+let currentUser = "";
+
+
+function login() {
+  let code = document.getElementById("code").value;
+
+  if (users[code]) {
+    currentUser = users[code];
+
+    document.getElementById("login").style.display = "none";
+    document.getElementById("app").style.display = "block";
+
+    document.getElementById("user").innerText = "Bem-vindo " + currentUser;
+
+  } else {
+    document.getElementById("error").innerText = "Código inválido!";
+  }
+}
+
+
+function send() {
+  let msg = document.getElementById("msg").value;
+
+  let chat = document.getElementById("chat");
+
+  let p = document.createElement("p");
+  p.innerText = currentUser + ": " + msg;
+
+  chat.appendChild(p);
+
+  document.getElementById("msg").value = "";
+}
